c9d8029027ba828d3ea726d42c2a85ea1766ba1e
Cahyo Dwi Putro <cahyorizqullah@gmail.com>
2024-03-20 22:43:21 +0700

    chore(init): :tada:

---
+++ README.md
@@ -0,0 +1,28 @@
+# EVM Crypto Brute Force
+This script is designed to automatically generate seed phrases, create wallets, and check balances for Ethereum, Binance Smart Chain (BSC), and Polygon (MATIC) networks. If a wallet with a non-zero balance is found, the wallet's information (address, mnemonic, private key, and balances) is logged and saved to a file named wallets.txt.
+
+![Preview](https://github.com/cdw1p/evm-crypto-bruteforce/blob/main/preview.png?raw=true)
+
+# Installation
+Make sure you have Node.js installed on your machine before running this script.
+- Clone this repository to your local machine.
+- Navigate to the project directory.
+- Run npm install to install the required dependencies.
+
+# Usage
+To start the brute force process, run the following command in the terminal:
+```bash
+node index.js
+```
+
+The script will continuously generate seed phrases and check the balances of the associated wallets. Information about each checked wallet will be logged to the console.
+If a wallet with a non-zero balance is found, its information will also be appended to wallets.txt in the project directory.
+
+# Disclaimer
+This script is provided for educational purposes only. Using this script to access wallets that you do not own may be illegal and unethical. The creator of this script is not responsible for any misuse or damage resulting from the use of this script.
+
+# Contributing
+If you have suggestions for improving this script, please feel free to submit a pull request or open an issue.
+
+# Reporting Issues
+If you encounter any issues while using this script, please report them by opening an issue in the repository.
---
+++ index.js
@@ -0,0 +1,108 @@
+const { ethers } = require('ethers')
+const axios = require('axios')
+const bip39 = require('bip39')
+const moment = require('moment')
+const cheerio = require('cheerio')
+const fs = require('fs-extra')
+require('colors')
+
+/**
+ * Delays the execution for the specified number of milliseconds.
+ * @param {number} ms - The number of milliseconds to delay the execution.
+ * @returns {Promise<void>} - A promise that resolves after the specified delay.
+ */
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+/**
+ * Logs a message to the console with a timestamp.
+ * @param {string} message - The message to be logged.
+ * @param {string} type - The type of message to log (e.g. 'info', 'error', 'warning').
+ */
+function logger(message, type) {
+  switch (type) {
+    case 'info':
+      console.log(`[${moment().format('HH:mm:ss')}] ${message}`)
+      break
+    case 'success':
+      console.log(`[${moment().format('HH:mm:ss')}] ${message}`.green)
+      break
+    case 'error':
+      console.error(`[${moment().format('HH:mm:ss')}] ${message}`.red)
+      break
+    case 'warning':
+      console.warn(`[${moment().format('HH:mm:ss')}] ${message}`.yellow)
+      break
+    default:
+      console.log(`[${moment().format('HH:mm:ss')}] ${message}`)
+  }
+}
+
+/**
+ * Generates a seed phrase using random bytes.
+ * @returns {string} The generated seed phrase.
+ */
+function generateSeedPhrase() {
+  const randomLength = Math.random() > 0.5 ? 24 : 12
+  const randomBytes = require('crypto').randomBytes(randomLength === 24 ? 32 : 16)
+  return bip39.entropyToMnemonic(randomBytes.toString('hex'))
+}
+
+/**
+ * Scrapes the Blockscan website to retrieve the balance of a given address.
+ * @param {string} address - The Ethereum address to scrape the balance for.
+ * @param {string} type - The type of address to scrape (e.g. 'eth
+ * @returns {Promise<string|boolean>} - A promise that resolves to the balance as a string, or false if an error occurs.
+ */
+function scrapeBlockscan(address, type = 'etherscan') {
+  const url = `https://${type}.com/address/${address}`
+  return axios.get(url)
+    .then(response => {
+      const $ = cheerio.load(response.data)
+      const balance = $('#ContentPlaceHolder1_divSummary > div.row.g-3.mb-4 > div:nth-child(1) > div > div > div:nth-child(3)').text()
+      return balance.split('\n')[4]
+    })
+    .catch(error => {
+      return false
+    })
+}
+
+/**
+ * Runs the brute force process to generate seed phrases, create wallets, and check balances.
+ * Logs the address, mnemonic, private key, and balance of each wallet checked.
+ * If a wallet with a non-zero balance is found, appends the wallet information to a file named 'wallets.txt'.
+ * @returns {Promise<void>} A promise that resolves when the brute force process is complete.
+ */
+async function runBruteforce() {
+  while (true) {
+    try {
+      const resSeedPhrase = generateSeedPhrase()
+      const resEtherWallet = ethers.Wallet.fromPhrase(resSeedPhrase)
+      const [resEthBalance, resBnbBalance, resMaticBalance] = await Promise.all([
+        scrapeBlockscan(resEtherWallet.address, 'etherscan'),
+        scrapeBlockscan(resEtherWallet.address, 'bscscan'),
+        scrapeBlockscan(resEtherWallet.address, 'polygonscan')
+      ])
+      logger(`👾 Address: ${resEtherWallet.address}`, 'info')
+      logger(`💬 Mnemonic: ${resEtherWallet.mnemonic.phrase}`, 'info')
+      logger(`🔑 Private key: ${resEtherWallet.privateKey}`, 'info')
+      logger(`🤑 ETH Balance: ${resEthBalance}`, 'info')
+      logger(`🤑 BNB Balance: ${resBnbBalance}`, 'info')
+      logger(`🤑 MATIC Balance: ${resMaticBalance}`, 'info')
+      if (resEthBalance !== '$0.00' || resBnbBalance !== '$0.00' || resMaticBalance !== '$0.00') {
+        logger(`🎉 Found a wallet with a non-zero balance!`, 'success')
+        await fs.appendFileSync('wallets.txt', `👾 Address: ${resEtherWallet.address}\n💬 Mnemonic: ${resEtherWallet.mnemonic.phrase}\n🔑 Private key: ${resEtherWallet.privateKey}\n🤑 ETH Balance: ${resEthBalance}\n🤑 BNB Balance: ${resBnbBalance}\n🤑 MATIC Balance: ${resMaticBalance}\n\n`)
+      } else {
+        logger(`👎 No luck this time.`, 'warning')
+      }
+      await delay(1000)
+    } catch (error) {
+      logger(`An error occurred: ${error.message}`, 'error')
+    }
+    console.log('')
+  }
+}
+
+/**
+ * Runs the bruteforce attack.
+ */
+runBruteforce()
